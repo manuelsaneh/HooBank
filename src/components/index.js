99bb5ef2c6f27072c3b4cd46001f2c6b3816edf3
Manuel Saneh <man.saneh10@gmail.com>
2023-01-08 19:52:31 +0200

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from './Billing'
+import Business from './Business'
+import Button from './Button'
+import CardDeal from './CardDeal'
+import Client from './Client'
+import CTA from './CTA'
+import Feedback from './Feedback'
+import Footer from './Footer'
+import GetStarted from './GetStarted'
+import Hero from './Hero'
+import Navbar from './Navbar'
+import Stats from './Stats'
+import Testimonials from './Testimonials'
+
+export {
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Client,
+  CTA,
+  Feedback,
+  Footer,
+  GetStarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+}
